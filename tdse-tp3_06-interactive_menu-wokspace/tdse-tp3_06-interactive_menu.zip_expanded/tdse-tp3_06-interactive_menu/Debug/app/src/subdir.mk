57e3a1140aa3ee49defa7aa50c6cc736fe27f67a
smabrera03 <santimcabrera@gmail.com>
2025-06-11 17:59:28 -0300

    Actualización 11/6 18:00

---
+++ tdse-tp3_06-interactive_menu-wokspace/tdse-tp3_06-interactive_menu.zip_expanded/tdse-tp3_06-interactive_menu/Debug/app/src/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../app/src/app.c \
+../app/src/display.c \
+../app/src/logger.c \
+../app/src/task_menu.c \
+../app/src/task_menu_interface.c \
+../app/src/task_sensor.c 
+
+OBJS += \
+./app/src/app.o \
+./app/src/display.o \
+./app/src/logger.o \
+./app/src/task_menu.o \
+./app/src/task_menu_interface.o \
+./app/src/task_sensor.o 
+
+C_DEPS += \
+./app/src/app.d \
+./app/src/display.d \
+./app/src/logger.d \
+./app/src/task_menu.d \
+./app/src/task_menu_interface.d \
+./app/src/task_sensor.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+app/src/%.o app/src/%.su app/src/%.cyclo: ../app/src/%.c app/src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../app/inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-app-2f-src
+
+clean-app-2f-src:
+	-$(RM) ./app/src/app.cyclo ./app/src/app.d ./app/src/app.o ./app/src/app.su ./app/src/display.cyclo ./app/src/display.d ./app/src/display.o ./app/src/display.su ./app/src/logger.cyclo ./app/src/logger.d ./app/src/logger.o ./app/src/logger.su ./app/src/task_menu.cyclo ./app/src/task_menu.d ./app/src/task_menu.o ./app/src/task_menu.su ./app/src/task_menu_interface.cyclo ./app/src/task_menu_interface.d ./app/src/task_menu_interface.o ./app/src/task_menu_interface.su ./app/src/task_sensor.cyclo ./app/src/task_sensor.d ./app/src/task_sensor.o ./app/src/task_sensor.su
+
+.PHONY: clean-app-2f-src
+
